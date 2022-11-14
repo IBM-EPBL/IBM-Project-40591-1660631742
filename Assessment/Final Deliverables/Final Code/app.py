1a5ab9eb51f9a25d0cb2f7d256861f7b0a9ea842
Vijayalakshmi R <89442922+Vijayalakshmi2019rit@users.noreply.github.com>
2022-11-14 15:28:17 +0530

    Create app.py

---
+++ Assessment/Final Deliverables/Final Code/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import sqlite3
+
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html', title='Home')
+
+
+@app.route('/analysis')
+def analysis():
+    return render_template('analysis.html', title='Analysis')
+
+@app.route('/classify')
+def classify():
+    return render_template('classify.html', title='Classify')
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html', title='Detail')
+
+
+
